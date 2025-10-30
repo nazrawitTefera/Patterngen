be5aaed66db0c8413b815de1d51d81a76cc21bdb
naz <nazikefyalew@gmail.com>
2025-10-30 17:59:04 +0000

    starter

--- Patterngen.java
+++ Patterngen.java
@@ -1,1 +1,11 @@
-
+//nazrawit tefera
+// october 30, 2025
+//pattern generator
+public class Pattern{
+    public static void stars(int rows){
+        //code goes here
+    }
+    public static void main(String[] args) {
+        stars(5);// precondition and post condition
+    }
+}
