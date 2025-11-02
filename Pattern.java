b6b0735e0bf0484f14e49fbc24464c88013b869b
naz <nazikefyalew@gmail.com>
2025-11-02 20:07:05 +0000

    done till the second part

---
+++ Pattern.java
@@ -0,0 +1,41 @@
+//nazrawit tefera
+// october 30, 2025
+//pattern generator
+public class Pattern{
+    //precondition-rows must be greater than zero
+    //postcondition- prints stars with the given number of rows
+    public static void stars(int rows){
+        int row=1;
+        int stars=1;
+        while(row<=rows){
+            int count=1;
+            while(count<= stars){
+                System.out.print("*");
+                count++;
+            }
+            System.out.println();
+            row++ ;
+            stars+=2 ;
+        }
+    }
+    //pre condition- the rows have to be greater than zero
+    //postconditin- prints a triangle formed numbers 
+    public static void triangle(int rows) {
+    int row = 1;  // start with row 1
+
+    while (row <= rows) {           
+        int count = 1;              
+
+        while (count <= row) {      
+            System.out.print(row);  
+            count++;
+        }
+
+        System.out.println();       
+        row++;                      
+    }
+}
+    public static void main(String[] args) {
+        stars(5);// precondition and post condition
+    }
+}
